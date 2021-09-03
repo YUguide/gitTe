1ad8d83a8f4126d005406e0edbe2bee025d4a84e
YUguide <73630971+YUguide@users.noreply.github.com>
2021-09-03 11:50:19 +0800

    pull commit

--- src/main/java/com/gogo/git/GitTest.java
+++ src/main/java/com/gogo/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("Hello git3");
         System.out.println("Hello master");
         System.out.println("Hello push");
+        System.out.println("Hello pull");
     }
 }
