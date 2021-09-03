1d742afcfa8dfb87523fc70dd79c264147d77c54
TIANCAI <TIANCAI@123.com>
2021-09-03 11:23:00 +0800

    first commit

---
+++ src/main/java/com/gogo/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.gogo.git;
+
+/**
+ * @author cy
+ * @create 2021-09-03 11:18
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git");
+    }
+}
